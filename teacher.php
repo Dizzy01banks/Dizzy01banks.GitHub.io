3cf5a09dcff4763a73b3c5f2541409884d972925
Dizzy01banks <127908815+Dizzy01banks@users.noreply.github.com>
2023-03-15 18:04:56 -0400

    Create teacher.php

---
+++ teacher.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teacher Portal</title>
+</head>
+<body>
+	<h1>Welcome, <?php echo $_POST['username']; ?>!</h1>
+	<h2>Your Gradebook</h2>
+	<!-- Insert code to display the teacher's gradebook here -->
+</body>
+</html>
