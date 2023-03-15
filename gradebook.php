a58c15630c4698e788bb221e593c1458a076a776
Dizzy01banks <127908815+Dizzy01banks@users.noreply.github.com>
2023-03-15 18:04:28 -0400

    Update gradebook.php

--- gradebook.php
+++ gradebook.php
@@ -28,3 +28,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	echo '<script>alert("Invalid credentials!")</script>';
 }
 ?>
+<?php
+
+// Define the valid accounts
+$accounts = array(
+  'teacher' => array(
+    'Teacher1' => 'Teach1',
+    'Teacher2' => 'Teach2',
+    'Teacher3' => 'Teach3'
+  ),
+  'student' => array(
+    'Student1' => 'Learn1',
+    'Student2' => 'Learn2'
+  )
+);
+
+// Get the form data
+$account_type = $_POST['account_type'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Check if the account is valid
+if (isset($accounts[$account_type]) && isset($accounts[$account_type][$username]) && $accounts[$account_type][$username] === $password) {
+  // Redirect to the appropriate personalized portal
+  if ($account_type === 'teacher') {
+    header('Location: teacher.php');
+    exit;
+  } else {
+    header('Location: student.php');
+    exit;
+  }
+} else {
+  // Invalid account, redirect back to the login page with an error message
+  header('Location: index.php?error=invalid');
+  exit;
+}
+
+?>
