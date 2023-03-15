4c3ec91a2624ea7ab39f879b95cd9936b565250b
Dizzy01banks <127908815+Dizzy01banks@users.noreply.github.com>
2023-03-15 18:05:21 -0400

    Create student.php

---
+++ student.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Portal</title>
+</head>
+<body>
+	<h1>Welcome, <?php echo $_POST['username']; ?>!</h1>
+	<h2>Your Grades</h2>
+	<!-- Insert code to display the student's grades here -->
+</body>
+</html>
